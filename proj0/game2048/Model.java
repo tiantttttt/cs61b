92df36ebbde5a38ab3cea4eccec35e10bafb4739
tiantttttt <xhttt@outlook.com>
2023-03-07 17:22:03 +0800

    proj0/22.3.7

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -138,6 +138,13 @@ public class Model extends Observable {
      * */
     public static boolean emptySpaceExists(Board b) {
         // TODO: Fill in this function.
+        for (int i = 0; i < b.size(); i++){
+            for (int j = 0; j < b.size(); j++){
+                if (b.tile(i, j) == null){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -148,6 +155,16 @@ public class Model extends Observable {
      */
     public static boolean maxTileExists(Board b) {
         // TODO: Fill in this function.
+        for (int i = 0; i < b.size(); i++){
+            for (int j = 0; j < b.size(); j++){
+                if (b.tile(i, j) == null){
+                    continue;
+                }
+                if (b.tile(i, j).value() == MAX_PIECE){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -159,6 +176,25 @@ public class Model extends Observable {
      */
     public static boolean atLeastOneMoveExists(Board b) {
         // TODO: Fill in this function.
+        for (int i = 0; i < b.size(); i++){
+            for (int j = 0; j < b.size(); j++){
+                if (b.tile(i, j) == null){
+                    return true;
+                }
+                if (i < 0 && b.tile(i - 1, j).value() == b.tile(i, j).value()){
+                   return true;
+                }
+                if (i < b.size() - 1 && b.tile(i + 1, j).value() == b.tile(i, j).value()) {
+                    return true;
+                }
+                if (j > 0 && b.tile(i, j - 1).value() == b.tile(i, j).value()) {
+                    return true;
+                }
+                if (j < b.size() - 1 && b.tile(i, j + 1).value() == b.tile(i, j).value()) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
