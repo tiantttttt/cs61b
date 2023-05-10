d9b79954a6938562dcb5dc657564af87ed9a1b63
tiantttttt <xhttt@outlook.com>
2023-05-10 23:37:59 +0800

    ??

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,7 +1,5 @@
 package deque;
 
-import org.hamcrest.internal.ArrayIterator;
-
 import java.util.Iterator;
 
 public class ArrayDeque<T> implements Deque<T> {
--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -1,5 +1,4 @@
 package deque;
-
 import java.util.Comparator;
 
 public class MaxArrayDeque<T> extends ArrayDeque<T> {
